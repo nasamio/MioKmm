a83203c0035bab2203972a6ee1a95fa317a3d402
nasamio <nasamio@163.com>
2024-12-23 18:03:06 +0800

    1.wasm加载网络图片

--- composeApp/src/commonMain/kotlin/com/mio/pages/test/Test.kt
+++ composeApp/src/commonMain/kotlin/com/mio/pages/test/Test.kt
@@ -10,8 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
 import kotlinx.coroutines.delay
+import miokmm.composeapp.generated.resources.Res
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun TestUi() {
     Column(
@@ -22,10 +27,20 @@ fun TestUi() {
             modifier = Modifier
                 .size(250.dp)
                 .padding(16.dp),
-            model = "https://img2.baidu.com/it/u=3068199002,3726366595&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=800",
+            model = "https://www4.bing.com//th?id=OHR.SantaClausVillage_ZH-CN1839275027_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp&w=360&h=202", // 加载res
             contentDescription = null,
             onError = { }
         )
+
+        AsyncImage(
+            modifier = Modifier
+                .size(250.dp)
+                .padding(16.dp),
+            model = Res.getUri("drawable/test.jpg"),
+            contentDescription = null,
+            onError = { }
+        )
+
 
         TestState.testList.forEach {
             Spacer(modifier = Modifier.height(10.dp))
