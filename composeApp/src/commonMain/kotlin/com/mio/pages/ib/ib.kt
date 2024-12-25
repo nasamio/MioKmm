e5372f67abe2da01440e38d785dc1c91d2239a3a
nasamio <nasamio@163.com>
2024-12-25 10:36:06 +0800

    1.大图查看

--- composeApp/src/commonMain/kotlin/com/mio/pages/ib/ib.kt
+++ composeApp/src/commonMain/kotlin/com/mio/pages/ib/ib.kt
@@ -1,6 +1,11 @@
 package com.mio.pages.ib
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -8,17 +13,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import bean.ib.IbFile
 import bean.smms.SmmsImage
 import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import utils.IbHelper
@@ -26,6 +32,7 @@ import utils.IbHelper
 @Composable
 fun IbUi() {
     val ibList = IbState.ibList.collectAsState()
+    var selectedImageUrl by remember { mutableStateOf<String?>(null) }
 
     LaunchedEffect(1) {
         IbHelper.apply {
@@ -39,22 +46,40 @@ fun IbUi() {
         }
     }
 
-    Column {
-        Title()
-        LazyVerticalGrid(
-            columns = GridCells.FixedSize(240.dp),
-            modifier = Modifier.fillMaxSize()
-                .background(color = Color.DarkGray),
-            contentPadding = PaddingValues(10.dp),
-            horizontalArrangement = Arrangement.spacedBy(10.dp), // 控制横向间隔
-            verticalArrangement = Arrangement.spacedBy(10.dp) // 控制纵向间隔
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(color = Color.DarkGray)
+    ) {
+        Column {
+            Title()
+            LazyVerticalGrid(
+                columns = GridCells.FixedSize(240.dp),
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(ibList.value.size) {
+                    IbItem(ibList.value[it]) { imageUrl ->
+                        selectedImageUrl = imageUrl
+                    }
+                }
+            }
+
+
+        }
+
+        // 显示大图
+        AnimatedVisibility(
+            visible = selectedImageUrl != null,
+            enter = fadeIn(animationSpec = tween(durationMillis = 300)), // 进入动画
+            exit = fadeOut(animationSpec = tween(durationMillis = 300)) // 退出动画
         ) {
-            items(ibList.value.size) {
-                IbItem(ibList.value[it])
+            FullScreenImageDialog(selectedImageUrl.toString()) {
+                selectedImageUrl = null // 关闭对话框
             }
         }
     }
-
 }
 
 @Composable
@@ -80,7 +105,7 @@ fun Title() {
 }
 
 @Composable
-fun IbItem(item: IbFile) {
+fun IbItem(item: IbFile, onImageClick: (String) -> Unit) {
     Column {
         Column(
             modifier = Modifier
@@ -91,12 +116,34 @@ fun IbItem(item: IbFile) {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             AsyncImage(
-                modifier = Modifier.size(240.dp),
+                modifier = Modifier
+                    .size(240.dp)
+                    .clickable { onImageClick(item.url) }, // 点击时传递图片URL
                 model = item.url,
                 contentDescription = null,
                 contentScale = ContentScale.FillWidth,
             )
             Text(text = item.filename, style = MaterialTheme.typography.body1)
         }
+    }
+}
+
+@Composable
+fun FullScreenImageDialog(imageUrl: String, onDismiss: () -> Unit) {
+    // 使用全屏对话框
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black) // 设置背景为黑色
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { onDismiss() } // 点击关闭对话框
+                .align(Alignment.Center) // 居中显示图片
+        )
     }
 }
